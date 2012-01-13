21bec997c899f59d77399f302b97adafd2d9bb80
Rachman Chavik <rchavik@xintesa.com>
2012-01-13 14:57:38 +0700

    inject menu when active

--- Plugin/Acl/Config/bootstrap.php
+++ Plugin/Acl/Config/bootstrap.php
@@ -3,14 +3,18 @@
 Croogo::hookBehavior('User', 'Acl.UserAco');
 Croogo::hookBehavior('Role', 'Acl.RoleAco');
 
-CroogoNav::add('users.children.permissions', array(
-	'title' => __('Permissions'),
-	'url' => array(
-		'admin' => true,
-		'plugin' => 'acl',
-		'controller' => 'acl_permissions',
-		'action' => 'index',
-		),
-	'access' => array('admin'),
-	'weight' => 30,
-	));
+if (Configure::read('Site.acl_plugin' == 'Acl')) {
+
+	CroogoNav::add('users.children.permissions', array(
+		'title' => __('Permissions'),
+		'url' => array(
+			'admin' => true,
+			'plugin' => 'acl',
+			'controller' => 'acl_permissions',
+			'action' => 'index',
+			),
+		'access' => array('admin'),
+		'weight' => 30,
+		));
+
+}
